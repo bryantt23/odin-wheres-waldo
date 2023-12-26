3b2e2a27f2a49d485c64a3484bf6dc1e9e627b7d
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-25 23:10:32 -0800

    format time & make sure dom updates first

--- script.js
+++ script.js
@@ -43,14 +43,21 @@ document.addEventListener('DOMContentLoaded', function () {
       if (allCharactersFound()) {
         img.style.opacity = '0.5'; // Set image opacity to 50%
         img.style.pointerEvents = 'none'; // Disable mouse events on the image
-        endGame();
+        setTimeout(endGame, 0); /*
+When you call setTimeout with a delay of 0, it doesn't execute the callback (in this case, endGame) immediately. Instead, it places the callback at the end of the event queue. This means that any JavaScript code that's currently executing will complete, and the browser will have a chance to update the DOM, process any user interactions, or handle other high-priority tasks before the endGame function is called.
+
+In the context of your application, this can be particularly useful because it allows the browser to apply any changes made to the DOM (like setting the opacity of the image and disabling pointer events) before the alert box is displayed. If you didn't use setTimeout, the alert could potentially block these updates from being rendered immediately, leading to a less smooth user experience.
+
+So, in summary, using setTimeout(endGame, 0) ensures that the UI updates (like fading out the image) are visually applied before the endGame function executes, which is responsible for showing the final alert. This technique leverages the browser's event loop to manage the timing of these operations.
+        */
       }
     }
   });
 
   function endGame() {
     clearInterval(timerInterval);
-    alert(`You found Waldo and friends in ${timeElapsed / 1000} seconds!`);
+    const seconds = (timeElapsed / 1000).toFixed(1); // Format the time in the same way as in startTimer
+    alert(`You found Waldo and friends in ${seconds} seconds!`);
   }
 
   function startTimer() {
