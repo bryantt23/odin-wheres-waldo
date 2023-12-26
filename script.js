a3c01ed74a619e661c848b21a736ec4f380871a9
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-25 22:15:20 -0800

    add hover

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   const img = document.getElementById('waldoImage');
+  const customCursor = document.getElementById('customCursor');
   const coordsDisplay = document.getElementById('coords');
 
   img.addEventListener('mousemove', function (event) {
     const rect = img.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    coordsDisplay.textContent = `X: ${x}, Y: ${y}`;
+    // Adjust for the custom cursor size and border
+    customCursor.style.left = x - 25 + 'px'; // Half the size of the cursor box to center
+    customCursor.style.top = y - 25 + 'px'; // Half the size of the cursor box to center
+    customCursor.style.display = 'block';
+    coordsDisplay.textContent = `X: ${x}, Y: ${y}`; // Update coordinates display
+  });
+
+  img.addEventListener('mouseleave', function (event) {
+    customCursor.style.display = 'none';
   });
 });
 
